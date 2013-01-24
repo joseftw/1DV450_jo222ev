68d4fe99f60db36ff6ae6abe5daa4e5c6b7dd2e0
Josef Ottosson <ottossonjosef@gmail.com>
2013-01-24 21:03:29 +0100

    First Commit
    Generated app

---
+++ RubyLabb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyLabb::Application.config.secret_token = 'feb4952419da61a67f8853ead8202a59b870051b8319b72bda1b10ed2137af9f7a3476caade45ca651552124b173ef9db6868b65535b3ee3d1f2fb60f6c0ba50'
---
+++ RubyLabb/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RubyLabb::Application.config.session_store :cookie_store, key: '_RubyLabb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RubyLabb::Application.config.session_store :active_record_store
