1eae5880a0b9fa3401130f9b09021a723fbfbb0a
Josef Ottosson <admin@Josefs-MacBook-Pro.local>
2013-02-17 18:09:20 +0100

    test

--- RubyLabb/config/routes.rb
+++ RubyLabb/config/routes.rb
@@ -13,9 +13,11 @@ RubyLabb::Application.routes.draw do
   # This route can be invoked with purchase_url(:id => product.id)
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
-  resources :projects
+  resources :projects do 
+    resources :tickets
+    end 
   resources :users
-  resources :tickets
+  #resources :tickets
   # Sample resource route with options:
   #   resources :products do
   #     member do
